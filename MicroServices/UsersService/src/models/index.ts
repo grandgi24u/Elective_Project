8b0f1647796bc53020fec978fa920d1f7cfb0af2
GRANDGIRARD <grandgirard.clement24@gmail.com>
2023-01-09 14:50:33 +0100

    Rajout des fichiers supprimer par Mr Valentin Deussen

---
+++ MicroServices/UsersService/src/models/index.ts
@@ -0,0 +1,34 @@
+// @ts-ignore
+const Sequelize = require("sequelize");
+// @ts-ignore
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mssql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    options: {
+        encrypt: false
+    }
+});
+
+// @ts-ignore
+const db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    user: require("../models/user.model.js")(sequelize, Sequelize),
+    role: require("../models/role.model.js")(sequelize, Sequelize)
+};
+
+db.user.belongsTo(db.role, {
+    foreignKey: 'roleId',
+    targetKey: 'id'
+})
+
+module.exports = db;
