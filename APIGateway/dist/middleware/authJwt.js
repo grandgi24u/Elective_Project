a977d1be648770786b66b92f59a323f6f714424e
GRANDGIRARD <grandgirard.clement24@gmail.com>
2023-01-11 17:21:06 +0100

    Ajout du système de connection + affichage des utilisateurs dans l'app c#

--- APIGateway/dist/middleware/authJwt.js
+++ APIGateway/dist/middleware/authJwt.js
@@ -4,6 +4,10 @@ const jwt = require("jsonwebtoken");
 // @ts-ignore
 const dotenv = require("dotenv");
 dotenv.config();
+// @ts-ignore
+const db = require("../models");
+// @ts-ignore
+const User = db.user;
 const verifyToken = (req, res, next) => {
     let token = req.headers["x-access-token"];
     if (!token) {
@@ -24,42 +28,52 @@ const verifyToken = (req, res, next) => {
 const isCustomer = (req, res, next) => {
     User.findByPk(req.userId).then((user) => {
         user.getRole().then((role) => {
-            if (role.name === "customer") {
-                next();
+            if (role.name !== "customer") {
+                res.status(403).send({
+                    message: "Accès refusé : vous n'êtes pas customer !"
+                });
                 return;
             }
-            res.status(403).send({
-                message: "Accès refusé : vous n'êtes pas customer !"
-            });
-            return;
+            next();
         });
     });
 };
 const isRestaurant = (req, res, next) => {
     User.findByPk(req.userId).then((user) => {
         user.getRole().then((role) => {
-            if (role.name === "restaurant") {
-                next();
+            if (role.name !== "restaurant") {
+                res.status(403).send({
+                    message: "Accès refusé : vous n'êtes pas un restaurateur !"
+                });
                 return;
             }
-            res.status(403).send({
-                message: "Accès refusé : vous n'êtes pas un restaurateur !"
-            });
-            return;
+            next();
         });
     });
 };
 const isDelivery = (req, res, next) => {
     User.findByPk(req.userId).then((user) => {
         user.getRole().then(role => {
-            if (role.name === "delivery") {
-                next();
+            if (role.name !== "delivery") {
+                res.status(403).send({
+                    message: "Accès refusé : vous n'êtes pas un livreur !"
+                });
                 return;
             }
-            res.status(403).send({
-                message: "Accès refusé : vous n'êtes pas un livreur !"
-            });
-            return;
+            next();
+        });
+    });
+};
+const isAdmin = (req, res, next) => {
+    User.findByPk(req.userId).then((user) => {
+        user.getRole().then(role => {
+            if (role.name !== "admin") {
+                res.status(403).send({
+                    message: "Accès refusé : vous n'êtes pas admin !"
+                });
+                return;
+            }
+            next();
         });
     });
 };
@@ -68,6 +82,7 @@ const authJwt = {
     verifyToken: verifyToken,
     isCustomer: isCustomer,
     isRestaurant: isRestaurant,
-    isDelivery: isDelivery
+    isDelivery: isDelivery,
+    isAdmin: isAdmin
 };
 module.exports = authJwt;
