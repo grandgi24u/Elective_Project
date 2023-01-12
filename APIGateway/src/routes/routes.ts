b9fe820efef442c80238fe9d76e4fa942d4d7615
Valentin DEUSSEN <valentin.deussen@viacesi.fr>
2023-01-12 11:15:43 +0100

    Fix routes in API GW for restaurant MS.

--- APIGateway/src/routes/routes.ts
+++ APIGateway/src/routes/routes.ts
@@ -53,14 +53,7 @@ const ROUTES = [
             onProxyReq: fixRequestBody,
             changeOrigin: true,
             pathRewrite: {
-                [`^/getrestaurant/getall`]: '/restaurant/',
-                [`^/getrestaurant/getone/:id`]: '/restaurant/:id',
-
-                [`^/getrestaurant/getone/:id/menu/getall`]: '/restaurant/:id/menu/',
-                [`^/getrestaurant/getone/:id/menu/getone/:idMenu`]: '/restaurant/:id/menu/:idMenu',
-
-                [`^/getrestaurant/getone/:id/item/getall`]: '/restaurant/:id/item/',
-                [`^/getrestaurant/getone/:id/item/getone/:idItem`]: '/restaurant/:id/item/:idItem',
+                [`^/getrestaurant`]: '/restaurant',
             },
         }
     },
@@ -73,26 +66,7 @@ const ROUTES = [
             onProxyReq: fixRequestBody,
             changeOrigin: true,
             pathRewrite: {
-                [`^/restaurant/create`]: '/restaurant/',
-                [`^/restaurant/patch/:id`]: '/restaurant/:id',
-                [`^/restaurant/delete/:id`]: '/restaurant/:id',
-
-                [`^/restaurant/:id/menu/create`]: '/restaurant/:id/menu/',
-                [`^/restaurant/:id/menu//patch/:idMenu`]: '/restaurant/:id/menu/:idMenu',
-                [`^/restaurant/:id/menu//delete/:idMenu`]: '/restaurant/:id/menu/:idMenu',
-
-                [`^/restaurant/:id/item/create`]: '/restaurant/:id/item/',
-                [`^/restaurant/:id/item//patch/:idItem`]: '/restaurant/:id/item/:idItem',
-                [`^/restaurant/:id/item//delete/:idItem`]: '/restaurant/:id/item/:idItem',
-
-                [`^/restaurant/:id/menu/:idMenu/createItem/required`]: '/restaurant/:id/menu/:idMenu/item_required/:idItem',
-                [`^/restaurant/:id/menu/:idMenu/createItem/optional`]: '/restaurant/:id/menu/:idMenu/item_optional/:idItem',
-
-                [`^/restaurant/:id/menu/:idMenu/bindItem/required`]: '/restaurant/:id/menu/:idMenu/bind_required_item/:idItem',
-                [`^/restaurant/:id/menu/:idMenu/bindItem/optional`]: '/restaurant/:id/menu/:idMenu/bind_optional_item/:idItem',
-
-                [`^/restaurant/:id/menu/:idMenu/unbindItem/required`]: '/restaurant/:id/menu/:idMenu/unbind_required_item/:idItem',
-                [`^/restaurant/:id/menu/:idMenu/unbindItem/optional`]: '/restaurant/:id/menu/:idMenu/unbind_optional_item/:idItem',
+                [`^/restaurant`]: '/restaurant',
             },
         }
     }
