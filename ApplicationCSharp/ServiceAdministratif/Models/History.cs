b30fb62b589b776362b6b07a2ddf506ee65ed4b2
GRANDGIRARD <grandgirard.clement24@gmail.com>
2023-01-17 21:00:49 +0100

    Changement de l'application c#

---
+++ ApplicationCSharp/ServiceAdministratif/Models/History.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceAdministratif.Models
+{
+    class History
+    {
+        public string Id { get; set; }
+        public string[] Id_menus { get; set; }
+        public string[] Id_items { get; set; }
+        public string Order_price { get; set; }
+        public DateTime Order_date { get; set; }
+        public string RestaurantId { get; set; }
+        public string DeliveryId { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ ApplicationCSharp/ServiceAdministratif/Pages/Order.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using ServiceAdministratif.Models;
+
+
+namespace ServiceAdministratif.Pages
+{
+    /// <summary>
+    /// Logique d'interaction pour Order.xaml
+    /// </summary>
+    public partial class Order : UserControl
+    {
+        private ApiOperations api = new ApiOperations();
+        public static Order instance;
+
+        public Order()
+        {
+            instance = this;
+            InitializeComponent();
+        }
+
+        public void InitGrid()
+        {
+            dgOrder.ItemsSource = JsonConvert.DeserializeObject<List<History>>(api.GetHistory());
+        }
+
+        public static void CallGrid()
+        {
+            instance.InitGrid();
+        }
+    }
+}
