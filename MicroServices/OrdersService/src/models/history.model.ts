cab80ace2d46944f5cc05bea429346ab8cc6ba8d
Geoffrey KEFF <geoffrey.keff@viacesi.fr>
2023-01-18 08:45:06 +0100

    changement type restaurantId, deliveryId et de id_menus dans historique

--- MicroServices/OrdersService/src/models/history.model.ts
+++ MicroServices/OrdersService/src/models/history.model.ts
@@ -8,11 +8,11 @@ const Schema = mongoose.Schema;
 const HistorySchema = new Schema({
     order_date: Date,
     order_price: String,
-    id_menus: [{ type: Schema.Types.ObjectId, ref: 'menu' }],
+    id_menus: [String],
     id_items: [{ type: Schema.Types.ObjectId, ref: 'item' }],
-    userid: Number,
-    deliveryId: Number,
-    restaurantId: Number,
+    userid: String,
+    deliveryId: String,
+    restaurantId: String,
 })
 
 
